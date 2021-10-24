1ed4805e3bdec13774462bee9e4a5e17405c1537
kyle-zechman <79478070+kyle-zechman@users.noreply.github.com>
2021-10-24 04:55:41 -0400

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -5,45 +5,38 @@ import java.util.*;
 public class Task2 {
 	
 	static String newTask = "";
-	
+	//method to create a new task
 	public static String add(String ID, String name, String description) {
+		//check if the ID for the task is empty and throw an exception if it is
 		if (ID == null) {
 			throw new IllegalArgumentException("Task ID is required");
 		}
+		//check if the ID for the task is longer than 10 characters and throw an exception if it is
 		if (ID.length() > 10) {
 			throw new IllegalArgumentException("Task ID cannot be longer than 10 characters");
 		}
-		
+		//check if the name for the task is empty and throw an exception if it is
 		if (name == null) {
 			throw new IllegalArgumentException("Task name is required");
 		}
+		//check if the name for the task is longer than 20 characters and throw an exception if it is
 		if (name.length() > 20) {
 			throw new IllegalArgumentException("Task name cannot be longer than 20 characters");
 		}
-		
+		//check if the description for the task is empty and throw an exception if it is
 		if (description == null) {
 			throw new IllegalArgumentException("Task description is required");
 		}
+		//check if the description for the task is longer than 50 characters and throw an exception if it is
 		if (description.length() > 50) {
 			throw new IllegalArgumentException("Task description cannot be longer than 50 characters");
 		}
-		
+		//create and return a new task based on the parameters passed to the method
 		newTask = ID + name + description;
 		return newTask;
 	}
-	
-	public String getTaskID(String ID) {
-		return ID;
-	}
-		
-	public String getTaskName(String name) {
-		return name;
-	}	
-		
-	public String getTaskDescription(String description) {
-		return description;
-	}
-	
+	//method for removing a task based on the parameter passed to the method
+	//throw an exception if the passed parameter doesn't match any appointment
 	public static void remove(String query) {
 		if (newTask.contains(query) == true) {
 			newTask = "";
@@ -52,7 +45,8 @@ public class Task2 {
 			throw new IllegalArgumentException("Task doesn't exist");
 		}
 	}
-	
+	//method for replacing a task based on the parameter passed to the method
+	//throw an exception if the passed parameter doesn't match any appointment
 	public static void replace(String oldContent, String newContent) {
 		if (newTask.contains(oldContent) == true) {
 			newTask = newContent;
