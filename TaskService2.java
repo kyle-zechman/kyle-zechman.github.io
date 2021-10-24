27ce71fcb73072b38a58d3f4927c05ca7e4f5bd3
kyle-zechman <79478070+kyle-zechman@users.noreply.github.com>
2021-10-24 04:56:55 -0400

    Update TaskService2.java

--- TaskService2.java
+++ TaskService2.java
@@ -4,15 +4,15 @@ import Task.Task2;
 import Task.Task;
 
 public class TaskService2 {
-	
+	//calls method to add task
 	public void addToList() {
 		Task2.add("ID", "name", "description");
 	}
-	
+	//calls method to remove task
 	public void deleteTask() {
 		Task2.remove("query");
 	}
-
+	//calls method to replace task
 	public void updateTask() {
 		Task2.replace("oldContent", "newContent");
 	}
