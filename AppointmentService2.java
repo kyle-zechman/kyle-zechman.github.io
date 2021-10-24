3761bf1ea961b3fd317a752464c231b690523a7d
kyle-zechman <79478070+kyle-zechman@users.noreply.github.com>
2021-10-24 04:58:36 -0400

    Update AppointmentService2.java

--- AppointmentService2.java
+++ AppointmentService2.java
@@ -3,11 +3,11 @@ import Appointment.Appointment2;
 import java.util.*;
 
 public class AppointmentService2 {
-
+	//calls method to add appointment
 	public void addToList() {
 		Appointment2.add("ID", null, "description");
 	}
-	
+	//calls method to remove appointment
 	public void deleteAppointment() {
 		Appointment2.remove("query");
 	}
