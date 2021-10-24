d0b050fd709322c6847d2b084636f3e6bfeb940c
kyle-zechman <79478070+kyle-zechman@users.noreply.github.com>
2021-10-24 04:57:39 -0400

    Update ContactService2.java

--- ContactService2.java
+++ ContactService2.java
@@ -3,15 +3,15 @@ import Contact.Contact2;
 import java.util.*;
 
 public class ContactService2 {
-	
+	//calls method to add contact
 	public void addToContactList() {
 		Contact2.add("ID", "firstName", "lastName", "phone", "address");
 	}
-	
+	//calls method to remove contact
 	public void deleteContact() {
 		Contact2.remove("query");
 	}
-
+	//calls method to update contact
 	public void updateContact() {
 		Contact2.replace("oldContent", "newContent");
 	}
